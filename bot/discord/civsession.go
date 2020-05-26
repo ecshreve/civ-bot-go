bd67d9ee0f4acdb28115b98df9f99518887357a8
Eric Shreve <ecshreve13@gmail.com>
2020-05-26 02:40:09 -0700

    go: move banCiv func to civsession file

--- bot/discord/civsession.go
+++ bot/discord/civsession.go
@@ -38,3 +38,23 @@ func (cs *CivSession) reset() {
 	cs.PickTime = time.Time{}
 	cs.RePickVotes = 0
 }
+
+// banCiv does a fuzzy match on the given string, if it finds a match it sets that
+// Civ's Banned value to true and updates the CivSession's slice of Bans.
+func banCiv(civToBan string, userID string) *Civ {
+	cs := Session
+	c := cs.getCivByString(civToBan)
+	if c == nil || c.Banned == true {
+		return nil
+	}
+
+	// If this player had previously banned a Civ then unban the previous Civ.
+	if _, ok := cs.Bans[userID]; ok {
+		cs.Bans[userID].Banned = false
+	}
+
+	c.Banned = true
+	cs.Bans[userID] = c
+
+	return c
+}
