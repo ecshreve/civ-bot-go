290410016918a3fbe35815b5fd899bd9c06e8ead
Eric Shreve <ecshreve13@gmail.com>
2020-05-26 02:36:46 -0700

    go: move ban instructions to reaction handler

--- bot/discord/ban.go
+++ bot/discord/ban.go
@@ -1,26 +1,5 @@
 package discord
 
-import (
-	"github.com/bwmarrin/discordgo"
-	"github.com/ecshreve/civ-bot-go/bot/constants"
-)
-
-// banInstructions sends a Message with instructions for how to ban a Civ.
-func banInstructions(s *discordgo.Session, channelID string) {
-	cs := Session
-	s.ChannelMessageSendEmbed(channelID, &discordgo.MessageEmbed{
-		Title:       "ℹ️ okay, here's our players",
-		Description: "- everyone gets to ban a civ now, enter `/civ ban <civ name>` to choose\n- if you change your mind just enter `/civ ban <new civ name>` to update your choice\n\nnote: you can enter a ban by either the civ or leader name",
-		Color:       constants.ColorGREEN,
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:  "Players",
-				Value: formatUsers(cs.Players),
-			},
-		},
-	})
-}
-
 // banCiv does a fuzzy match on the given string, if it finds a match it sets that
 // Civ's Banned value to true and updates the CivSession's slice of Bans.
 func banCiv(civToBan string, uid string) *Civ {
--- bot/discord/reaction_handlers.go
+++ bot/discord/reaction_handlers.go
@@ -1,6 +1,9 @@
 package discord
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/ecshreve/civ-bot-go/bot/constants"
+)
 
 // newReactionHandler handles all new related reactions.
 func newReactionHandler(s *discordgo.Session, r *discordgo.MessageReactionAdd, m *discordgo.Message, user *discordgo.User) {
@@ -8,7 +11,17 @@ func newReactionHandler(s *discordgo.Session, r *discordgo.MessageReactionAdd, m
 		Session.Players[user.ID] = user
 	}
 	if r.Emoji.Name == "✅" {
-		banInstructions(s, m.ChannelID)
+		s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+			Title:       "ℹ️ okay, now that we have our players",
+			Description: "- everyone gets to ban a civ, enter `/civ ban <civ name>` to choose\n- if you change your mind just enter `/civ ban <new civ name>` to update your choice\n\nnote: you can enter a ban by either the civ or leader name",
+			Color:       constants.ColorGREEN,
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:  "Players",
+					Value: formatUsers(Session.Players),
+				},
+			},
+		})
 	}
 }
 
