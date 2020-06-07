02fc492dc21060ea3706cdc11dd3987ac4b4484d
Eric Shreve <ecshreve13@gmail.com>
2020-06-07 00:57:01 -0700

    go test: use snapshotter package
    this makes tests less verbose and easier to understand

--- internal/bot/civstate_test.go
+++ internal/bot/civstate_test.go
@@ -2,29 +2,16 @@ package bot_test
 
 import (
 	"testing"
-	"time"
 
-	"github.com/ecshreve/civ-bot-go/internal/constants"
+	"github.com/samsarahq/go/snapshotter"
 
 	"github.com/ecshreve/civ-bot-go/internal/bot"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestNewState(t *testing.T) {
-	state := bot.NewCivState()
-	assert.NotNil(t, state)
+	snap := snapshotter.New(t)
+	defer snap.Verify()
 
-	assert.Nil(t, state.Players)
-	assert.Nil(t, state.PlayerMap)
-
-	assert.Equal(t, len(constants.CivKeys), len(state.Civs))
-	assert.Equal(t, len(constants.CivKeys), len(state.CivMap))
-
-	assert.Nil(t, state.Bans)
-	assert.Nil(t, state.Picks)
-
-	assert.NotNil(t, state.PickState)
-	assert.Equal(t, time.Time{}, state.PickState.PickTime)
-	assert.Equal(t, 0, state.PickState.RePickVotes)
-	assert.Equal(t, bot.DefaultCivConfig.RePicks, state.PickState.RePicksRemaining)
+	output := bot.NewCivState()
+	snap.Snapshot("default civ state", output)
 }
