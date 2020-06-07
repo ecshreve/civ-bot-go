a7e1fdfb9fcd5073825ecf8a188ddade108d1af6
Eric Shreve <ecshreve13@gmail.com>
2020-06-06 22:52:43 -0700

    bot command: document commands.go

--- internal/bot/commands.go
+++ internal/bot/commands.go
@@ -2,22 +2,28 @@ package bot
 
 import (
 	"github.com/bwmarrin/discordgo"
+
 	"github.com/ecshreve/civ-bot-go/internal/constants"
 )
 
+// CommandID is a wrapper around the string representation of the command.
 type CommandID string
 
+// CommandInfo defines the properties of a Command.
 type CommandInfo struct {
 	Name        string
 	Emoji       string
 	Description string
 	Usage       string
 }
+
+// Command is any command sent to the Bot.
 type Command interface {
 	Info() *CommandInfo
 	Process(*discordgo.Message) *discordgo.MessageEmbed
 }
 
+// Command interface implementation for the "help" command.
 type helpCommand struct{}
 
 func (c *helpCommand) Info() *CommandInfo {
